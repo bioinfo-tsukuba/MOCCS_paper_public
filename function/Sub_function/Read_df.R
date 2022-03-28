24945fc79a311235afe2c261d101add66f11ed14
t-takaho <takaho.tsuchiya@gmail.com>
2022-03-28 15:12:38 +0900

    Add functions

--- function/Sub_function/Read_df.R
+++ function/Sub_function/Read_df.R
@@ -1,5 +1,7 @@
 Read_df <- function(){
 
+  library(dplyr)
+  
   # Get MOCCSout_hg38_hard_filter_annotated.rds
   system("wget https://figshare.com/ndownloader/files/34065695 -P ~/MOCCS-DB_paper/data/Fig2/out")
   system("mv ~/MOCCS-DB_paper/data/Fig2/out/34065695 ~/MOCCS-DB_paper/data/Fig2/out/MOCCSout_hg38_hard_filter_annotated.rds")
@@ -8,7 +10,7 @@ Read_df <- function(){
   system("wget https://figshare.com/ndownloader/files/34065689 -P ~/MOCCS-DB_paper/data/Fig2/qval")
   system("mv ~/MOCCS-DB_paper/data/Fig2/qval/34065689 ~/MOCCS-DB_paper/data/Fig2/qval/MOCCSout_hg38_all_qval.rds")
   
-  df_raw_pre <- readRDS("~/MOCCS-DB_paper/data/Fig2/MOCCSout_hg38_hard_filter_annotated.rds")
+  df_raw_pre <- readRDS("~/MOCCS-DB_paper/data/Fig2/out/MOCCSout_hg38_hard_filter_annotated.rds")
   df_raw_pre %>% mutate(ID_kmer = paste0(ID, kmer)) -> df_raw_pre_2
   df_qval <- readRDS("~/MOCCS-DB_paper/data/Fig2/qval/MOCCSout_hg38_all_qval.rds")
   df_qval %>% mutate(ID_kmer = paste0(ID, kmer)) -> df_qval_2
