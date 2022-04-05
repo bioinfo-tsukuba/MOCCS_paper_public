ec94456b03fde0d1a9a49044e0f83fe8e6559e89
t-takaho <takaho.tsuchiya@gmail.com>
2022-04-05 13:11:56 +0900

    Update Fig2_3_plot.R

--- function/Fig2_3_plot.R
+++ function/Fig2_3_plot.R
@@ -2,9 +2,6 @@ Fig2_3_plot <- function(calc_opt = TRUE){
   
   library(dplyr)
   
-  # Get family annotation of all sample pairs
-  # Get annotation of all sample pairs
-  # Calculate k-sim 1/2 between all sample pairs
   if (calc_opt){
 
     source("~/MOCCS_paper_public/function/sub_function/Read_df.R")
@@ -40,6 +37,11 @@ Fig2_3_plot <- function(calc_opt = TRUE){
     df_poi <- Collect_poi()
     saveRDS(df_poi, "~/MOCCS_paper_public/data/Fig2/obj/df_poi.rds")
     
+    # Get ID list whose k-mers are all non-significant
+    source("~/MOCCS_paper_public/function/sub_function/Get_ans.R")
+    all_ns <- Get_ans(df_raw)
+    saveRDS(all_ns, "~/MOCCS_paper_public/data/Fig2/obj/all_ns.rds")
+    
   } else {
     
     df_raw <- readRDS("~/MOCCS_paper_public/data/Fig2/obj/df_raw.rds") # FIMXE: figshare
@@ -49,46 +51,46 @@ Fig2_3_plot <- function(calc_opt = TRUE){
     df_p_3 <- readRDS("~/MOCCS_paper_public/data/Fig2/obj/df_p_3.rds")
     df_p_3_gp <- readRDS("~/MOCCS_paper_public/data/Fig2/obj/df_p_3_gp.rds")
     df_poi <- readRDS("~/MOCCS_paper_public/data/Fig2/obj/df_poi.rds") # FIXME: figshare
+    all_ns <- readRDS("~/MOCCS_paper_public/data/Fig2/obj/all_ns.rds")
     
   }
+  
 
   # Fig. 2B and S4: Comparison with poi
   source("~/MOCCS_paper_public/function/sub_function/Compare_ksim_poi.R")
-  Compare_ksim_poi(df_p_3_gp, df_poi)
+  flag_1 <- !(df_p_3_gp$ID1 %in% all_ns | df_p_3_gp$ID2 %in% all_ns)
+  flag_2 <- !(df_poi$ID1 %in% all_ns | df_poi$ID2 %in% all_ns)
+  Compare_ksim_poi(df_p_3_gp[flag_1, ], df_poi[flag_2, ])
   
   # Fig. 2C and 3B: UMAP
   source("~/MOCCS_paper_public/function/sub_function/Umap_df.R")
-  UMAP_df(df_raw, df_fam)
+  UMAP_df(df_raw, df_fam, all_ns)
   
   # Fig 2D, 3C and S5: Permutation and Chi squared tests
   source("~/MOCCS_paper_public/function/sub_function/Top_k_sim.R")
-  Top_k_sim(df_p_1, df_p_2, calc_opt = FALSE)
+  flag_3 <- !(df_p_1$ID1 %in% all_ns | df_p_1$ID2 %in% all_ns)
+  flag_4 <- !(df_p_2$ID1 %in% all_ns | df_p_2$ID2 %in% all_ns)
+  Top_k_sim(df_p_1[flag_3, ], df_p_2[flag_4, ], calc_opt = TRUE, perm_num = 1000)
   
-  # Fig. 2E: TF graph
-  source("~/MOCCS_paper_public/function/sub_function/TF_graph.R")
-  TF_graph(df_p_3)
-  
-  # Fig. 3X
+  # Fig. 2E
   source("~/MOCCS_paper_public/function/sub_function/TF_graph_2.R")
-  TF_graph_2(df_p_3)
-  source("~/MOCCS_paper_public/function/sub_function/TF_graph_3.R")
-  TF_graph_3(df_p_3) # k-sim 1
+  flag_5 <- !(df_p_3$ID1 %in% all_ns | df_p_3$ID2 %in% all_ns)
+  TF_graph_2(df_p_3[flag_5, ])
   source("~/MOCCS_paper_public/function/sub_function/TF_graph_sel.R")
-  TF_graph_sel(ctc_spe = NULL, df_p_3)
-  source("~/MOCCS_paper_public/function/sub_function/TF_graph_sel_2.R")
-  TF_graph_sel_2(rt_list = c("FOS", "JUN"), t_ctc = "Breast")
-  TF_graph_sel_2(rt_list = c("FOS", "JUN"), t_ctc = "Cardiovascular")
-  source("~/MOCCS_paper_public/function/sub_function/TF_graph_sel_3.R")
-  sig_symbol_list <- readRDS("~/MOCCS_paper_public/data/Fig2/obj/sig_symbol_list.rds")
-  sig_symbol_list_2 <- sig_symbol_list[!sig_symbol_list %in% c("AR", "FOS")]
-  for (a1_ind in 1:length(sig_symbol_list_2)){
-    TF_graph_sel_3(a1 = sig_symbol_list_2[a1_ind], t_ctc = NULL, t_num = 15)
-  }
-
+  TF_graph_sel(ctc_spe = NULL, df_p_3[flag_5, ])
+  
   # Fig. 3D and 3E: Cell type comparison
-  plot_ant <- c("FOS", "JUN", "CTCF", "GATA2", "MYC")
   source("~/MOCCS_paper_public/function/sub_function/Compare_cell_type.R")
-  Compare_cell_type(df_p_3_gp, plot_ant, skip_opt = TRUE)
+  flag_1 <- !(df_p_3_gp$ID1 %in% all_ns | df_p_3_gp$ID2 %in% all_ns)
+  Compare_cell_type(df_p_3_gp[flag_1, ],
+                    plot_ant = c("FOS", "JUN", "GATA2", "MYC"))
+  
+  # Fig. 3F
+  source("~/MOCCS_paper_public/function/sub_function/TF_graph_3.R")
+  TF_graph_3(df_p_3)
+  source("~/MOCCS_paper_public/function/sub_function/TF_graph_sel_3.R")
+  TF_graph_sel_3(a1 = "JUN", t_ctc = NULL, t_num = 15)
+  TF_graph_sel_3(a1 = "GATA2", t_ctc = NULL, t_num = 15)
   
 
   return()
